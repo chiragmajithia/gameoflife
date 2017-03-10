e8449a6c56073cd5873fa04170bf43de1420e041
chirag <chiragamajithia@gmail.com>
2017-03-09 22:35:36 -0500

    seq 2 version CMakeUpdate

--- build/CMakeFiles/life_seq_opt.dir/DependInfo.cmake
+++ build/CMakeFiles/life_seq_opt.dir/DependInfo.cmake
@@ -4,19 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/homes/cmsc714-1bv3/hpc/p1/src/life_seq_opt.cpp" "/homes/cmsc714-1bv3/hpc/p1/build/CMakeFiles/life_seq_opt.dir/src/life_seq_opt.cpp.o"
+  "/home/chirag/UMD Courses/highperformancecomputing/p1_remote/gameoflife/src/life_seq_opt.cpp" "/home/chirag/UMD Courses/highperformancecomputing/p1_remote/gameoflife/build/CMakeFiles/life_seq_opt.dir/src/life_seq_opt.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MPI_INCLUDE_PATH"
+  "../include"
+  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../MPI_INCLUDE_PATH"
-  "../include"
-  )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /homes/cmsc714-1bv3/hpc/p1
+# Install script for directory: /home/chirag/UMD Courses/highperformancecomputing/p1_remote/gameoflife
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -38,7 +38,7 @@ else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-file(WRITE "/homes/cmsc714-1bv3/hpc/p1/build/${CMAKE_INSTALL_MANIFEST}" "")
-foreach(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  file(APPEND "/homes/cmsc714-1bv3/hpc/p1/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
-endforeach()
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/chirag/UMD Courses/highperformancecomputing/p1_remote/gameoflife/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
